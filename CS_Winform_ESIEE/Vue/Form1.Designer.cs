ccf0e7ff1a066708f57506db5f37f9c9c54c6a4b
Hajïn_ <44238262+AHajin@users.noreply.github.com>
2024-12-05 13:49:58 +0100

    Update : commentaire
    - Ajout de commentaires

--- CS_Winform_ESIEE/Vue/Form1.Designer.cs
+++ CS_Winform_ESIEE/Vue/Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Articles = new System.Windows.Forms.ListBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
@@ -44,7 +44,7 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.Categories = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
             this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
             this.button5 = new System.Windows.Forms.Button();
@@ -60,14 +60,15 @@
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // listBox1
+            // Articles
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(25, 91);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(422, 251);
-            this.listBox1.TabIndex = 0;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.Articles.AccessibleName = "articlesBox";
+            this.Articles.FormattingEnabled = true;
+            this.Articles.Location = new System.Drawing.Point(25, 91);
+            this.Articles.Name = "Articles";
+            this.Articles.Size = new System.Drawing.Size(422, 251);
+            this.Articles.TabIndex = 0;
+            this.Articles.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // groupBox1
             // 
@@ -121,6 +122,7 @@
             this.label5.Size = new System.Drawing.Size(59, 15);
             this.label5.TabIndex = 11;
             this.label5.Text = "Quantités";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label4
             // 
@@ -183,12 +185,13 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(67, 198);
+            this.button3.Location = new System.Drawing.Point(78, 198);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 4;
-            this.button3.Text = "button3";
+            this.button3.Text = "Modifier";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // textBox3
             // 
@@ -196,6 +199,7 @@
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(54, 20);
             this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // textBox2
             // 
@@ -203,6 +207,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(68, 20);
             this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox1
             // 
@@ -212,13 +217,14 @@
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // listBox2
+            // Categories
             // 
-            this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(25, 29);
-            this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(657, 56);
-            this.listBox2.TabIndex = 2;
+            this.Categories.FormattingEnabled = true;
+            this.Categories.Location = new System.Drawing.Point(25, 29);
+            this.Categories.Name = "Categories";
+            this.Categories.Size = new System.Drawing.Size(657, 56);
+            this.Categories.TabIndex = 2;
+            this.Categories.SelectedIndexChanged += new System.EventHandler(this.Categories_SelectedIndexChanged);
             // 
             // button1
             // 
@@ -287,7 +293,7 @@
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             // 
             // editToolStripMenuItem
@@ -295,6 +301,7 @@
             this.editToolStripMenuItem.Name = "editToolStripMenuItem";
             this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
             this.editToolStripMenuItem.Text = "Edit";
+            this.editToolStripMenuItem.Click += new System.EventHandler(this.editToolStripMenuItem_Click);
             // 
             // aboutToolStripMenuItem
             // 
@@ -311,9 +318,9 @@
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.Categories);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Articles);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
@@ -330,12 +337,12 @@
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox Articles;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox Categories;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
--- CS_Winform_ESIEE/Vue/Form1.cs
+++ CS_Winform_ESIEE/Vue/Form1.cs
@@ -27,10 +27,16 @@ namespace CS_Winform_ESIEE
 
         }
 
+        /*
+         * Bouton de passage en mode réapprovisionnement
+         */
         private void button1_Click(object sender, EventArgs e)
         {
         }
 
+        /*
+         * Champ de text du nom de l'article
+         */
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
@@ -41,6 +47,10 @@ namespace CS_Winform_ESIEE
 
         }
 
+
+        /**
+         * Bouton d'application/suppression de la remise
+         */
         private void button4_Click(object sender, EventArgs e)
         {
             if (button4.Text == "Appliquer")
@@ -50,26 +60,41 @@ namespace CS_Winform_ESIEE
                 
         }
 
+        /*
+         * Label "Articles"
+         */
+
         private void label2_Click(object sender, EventArgs e)
         {
 
         }
 
+        /**
+         * Label prix 
+         */
         private void label4_Click(object sender, EventArgs e)
         {
 
         }
 
+        /**
+         * Groupe de détails d'articles 
+         */
         private void groupBox1_Enter(object sender, EventArgs e)
         {
 
         }
 
+        /**
+         * Boutton d'historique 
+         */
         private void button5_Click(object sender, EventArgs e)
         {
 
         }
-
+        /*
+         * Liste d'articles
+         */
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
@@ -80,9 +105,59 @@ namespace CS_Winform_ESIEE
 
         }
 
+        /**
+         * Barre de Navigation File -> Exit 
+         */
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+
+        /*
+         * Liste de catégories
+         */
+        private void Categories_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /*
+         * Champ de text de la remise
+         */
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /**
+         * Champ de text de la quantité
+         */
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /**
+         * Label Quantités 
+         */
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /**
+         * Bouton modifier l'article 
+         */
+        private void button3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
