96b75e358c25eead742026255817e2bc407b4992
Redstoneur <alfa.aog36@gmail.com>
2024-12-06 15:05:24 +0100

    Feature : Modele documentation
    ADD Doxygen

--- CS_Winform_ESIEE/Business/PanierController.cs
+++ CS_Winform_ESIEE/Business/PanierController.cs
@@ -2,15 +2,22 @@
 
 namespace CS_Winform_ESIEE.Business
 {
+    /// <summary>
+    /// La classe PanierController gère les opérations liées au panier d'achat (Panier).
+    /// </summary>
     public class PanierController
     {
+        /// <summary>
+        /// Passe une commande basée sur les articles dans le panier d'achat fourni (Panier).
+        /// </summary>
+        /// <param name="panier">Le panier d'achat contenant les articles à commander.</param>
         public void Commander(Panier panier)
         {
-            // Ajouter la commande
+            // Créer une nouvelle commande
             CommandeController commandeController = new CommandeController();
             long idCommande = commandeController.AjouterCommande();
 
-            // transformer la liste d'articles en lignes de commande et les ajouter
+            // Convertir la liste des articles en lignes de commande et les ajouter
             LigneCommandeController ligneCommandeController = new LigneCommandeController();
             foreach (Article article in panier.GetArticles())
             {
